9f8349e78aa46cf88709d0cb0f6d8ee0f43590e3
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-21 08:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function mincost(arr)
 { 
+
+	arr.sort((a, b) => a - b);
+
+	let cost = 0;
+	while (arr.length > 1) {
+
+		let first = arr.shift();
+		let second = arr.shift();
+		let sum = first + second;
+
+		cost += sum;
+
+		arr.push(sum);
+
+		arr.sort((a, b) => a - b);
+	}
+	
 //write your code here
 // return the min cost
   
